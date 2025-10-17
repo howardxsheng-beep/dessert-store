f8c08b9d18990578bc0a73bea672fa1efe699a8f
ho <howardxsheng@gmail.com>
2025-10-17 12:39:20 +0800

    commit init

---
+++ src/pages/SiteHeader.jsx
@@ -0,0 +1,13 @@
+
+function SiteHeader(){
+   return(
+   <>
+     <header>
+        <div class="h-[84px] ms:mx-auto ms:w-full ms:max-w-[940px] ms:h-[100px]">
+            eeqw
+        </div>
+     </header>
+    </> )
+}
+
+export default SiteHeader;
