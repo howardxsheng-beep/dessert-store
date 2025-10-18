00ecf58c9b18e1a578a49f4180b5e3e412221b7e
ho <howardxsheng@gmail.com>
2025-10-18 15:42:20 +0800

    feat:設定color css 新開mail資料

---
+++ src/pages/MailDist.jsx
@@ -0,0 +1,10 @@
+function MailDist(){
+    return(
+        <>
+        <div className="w-full bg-">
+            <h1 className="bg-dark-green">test</h1>
+        </div>
+        </>
+    );
+}
+export default MailDist;
--- src/pages/SiteHeader.jsx
+++ src/pages/SiteHeader.jsx
@@ -1,43 +1,62 @@
-
-function SiteHeader(){
-   return(
-   <>
-     <header>
-        <div className="h-[84px] w-full mx-auto max-w-[640px]
-         sm:mx-auto sm:w-full sm:max-w-[940px] sm:h-[100px]">
-            <div className="flex items-center justify-between h-full ">
-                {/* 左邊選單 */}
-                <div className="flex  items-center w-[24px] h-[24px] ml-[30px] sm:hidden">
-                    <img src="./src/images/dehaze-24px.png" className="h-[24px] w-auto object-cover"></img>
-                </div>
-               
-                {/* 中間logo */}
-                <div className="flex items-center w-[114px] h-[18px]">
-                    <img src="./src/images/logo-all-dark.svg" 
-                    className="hidden h-[24px]   sm:block"></img>
-
-                    <img src="./src/images/logotype-sm-dark.svg" 
-                    className="sm:hidden h-[17px] "></img>
-                </div>
-                {/* 右邊購物車 */}
-              
-
-                <div className="flex mr-[30px]">
-                    <nav className="hidden sm:flex sm:justify-items-end" aria-label="mainn-nav">
-                        <ul className="flex sm:items-end gap-8  text-[#2c5c47]">
-                            <li><a href="#">首頁</a></li>
-                            <li><a href="#">甜點</a></li>
-                            <li><a href="#">登入</a></li>
-                        </ul>
-                    </nav>
-                    <div className="flex justify-center sm:justify-end sm:grid  w-[24px] h-[24px] ">
-                        <img src="./src/images/shopping_cart.png"></img>
-                    </div>
-                </div>
+function SiteHeader() {
+  return (
+    <>
+      <header>
+        <div
+          className=" h-[84px] w-full mx-auto max-w-[640px]
+         sm:mx-auto sm:w-full sm:max-w-[940px] sm:h-[100px]"
+        >
+          <div className="flex items-center justify-between h-full ">
+            {/* 左邊選單 */}
+            <div className="flex  items-center w-[24px] h-[24px] ml-[30px] shrink-0 sm:hidden">
+              <img
+                src="./src/images/dehaze-24px.png"
+                className="h-[24px] w-auto object-cover"
+              ></img>
             </div>
+
+            {/* 中間logo */}
+            <div className="flex items-center w-auto shrink-0 h-[18px]">
+              <img
+                src="./src/images/logo-all-dark.svg"
+                className="hidden h-[40px] w-auto   sm:block"
+              ></img>
+
+              <img
+                src="./src/images/logotype-sm-dark.svg"
+                className="sm:hidden h-[17.74px] w-auto "
+              ></img>
+            </div>
+            {/* 右邊購物車 */}
+
+            <div className="flex mr-[30px] sm:mr-0">
+              <nav
+                className="hidden sm:flex sm:justify-items-end"
+
+              >
+                <ul className="flex sm:items-end gap-[60px] pr-[79px] text-[#3F5D45]">
+                  <li>
+                    <a href="#">首頁</a>
+                  </li>
+                  <li>
+                    <a href="#">甜點</a>
+                  </li>
+                  <li>
+                    <a href="#">登入</a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="flex justify-center sm:justify-end sm:grid  w-[24px] h-[24px] ">
+                <a href="#">
+                <img src="./src/images/shopping_cart.png"></img>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-     </header>
-    </> )
+      </header>
+    </>
+  );
 }
 
 export default SiteHeader;
