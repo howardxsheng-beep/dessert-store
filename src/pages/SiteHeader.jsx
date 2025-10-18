157517dfbc182cdfa017cf8d4615064bdd639f3f
ho <howardxsheng@gmail.com>
2025-10-18 11:30:32 +0800

    feat: 切版位置完成

--- src/pages/SiteHeader.jsx
+++ src/pages/SiteHeader.jsx
@@ -3,8 +3,38 @@ function SiteHeader(){
    return(
    <>
      <header>
-        <div class="h-[84px] ms:mx-auto ms:w-full ms:max-w-[940px] ms:h-[100px]">
-            eeqw
+        <div className="h-[84px] w-full mx-auto max-w-[640px]
+         sm:mx-auto sm:w-full sm:max-w-[940px] sm:h-[100px]">
+            <div className="flex items-center justify-between h-full ">
+                {/* 左邊選單 */}
+                <div className="flex  items-center w-[24px] h-[24px] ml-[30px] sm:hidden">
+                    <img src="./src/images/dehaze-24px.png" className="h-[24px] w-auto object-cover"></img>
+                </div>
+               
+                {/* 中間logo */}
+                <div className="flex items-center w-[114px] h-[18px]">
+                    <img src="./src/images/logo-all-dark.svg" 
+                    className="hidden h-[24px]   sm:block"></img>
+
+                    <img src="./src/images/logotype-sm-dark.svg" 
+                    className="sm:hidden h-[17px] "></img>
+                </div>
+                {/* 右邊購物車 */}
+              
+
+                <div className="flex mr-[30px]">
+                    <nav className="hidden sm:flex sm:justify-items-end" aria-label="mainn-nav">
+                        <ul className="flex sm:items-end gap-8  text-[#2c5c47]">
+                            <li><a href="#">首頁</a></li>
+                            <li><a href="#">甜點</a></li>
+                            <li><a href="#">登入</a></li>
+                        </ul>
+                    </nav>
+                    <div className="flex justify-center sm:justify-end sm:grid  w-[24px] h-[24px] ">
+                        <img src="./src/images/shopping_cart.png"></img>
+                    </div>
+                </div>
+            </div>
         </div>
      </header>
     </> )
