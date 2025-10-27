17d9cdaf79bd30067bbbbc54572dd6a66ea95473
ho <howardxsheng@gmail.com>
2025-10-27 09:51:33 +0800

    fix:整理縮排

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -5,7 +5,7 @@ function CheckoutSuccess() {
         {/* 背景圖 */}
         <div className="relative w-full h-[518px] sm:h-[460px] ">
           <img
-            src="/images/checkout-success.png"   // 手機版圖
+            src="/images/checkout-success.png"   
 
             className="absolute inset-0 w-full h-full object-cover "
           />
@@ -17,23 +17,21 @@ function CheckoutSuccess() {
             <div className="flex items-center  mb-8">
               <span className="w-5 h-5 grid place-items-center rounded-full bg-dark-green text-ink-inverse place-content-center">✓</span>
                 <span aria-hidden className=" h-px w-[65px] bg-dark-green"></span>
-
               <span className="w-5 h-5 grid place-items-center rounded-full bg-dark-green text-ink-inverse  place-content-center">✓</span>
                 <span aria-hidden className=" h-px w-[65px] bg-dark-green"></span>
-
               <span className="w-5 h-5 grid place-items-center rounded-full bg-dark-green text-ink-inverse  place-content-center">✓</span>
             </div>
 
             <img src="images/lg-付款成功@2x.png" className="max-w-[190px] w-full mb-13"></img>
 
-            {/* 桌機版：內嵌按鈕 */}
+            {/* 桌機版按鈕 */}
             <button className="hidden w-full sm:max-w-[300px] sm:inline-flex justify-center items-center mt-2 h-[65px]  bg-light-yellow text-dark-green text-xl font-semibold">
               繼續逛逛
             </button>
           </div>
         </div>
 
-        {/* 手機版：底部滿寬按鈕 */}
+        {/* 手機按鈕 */}
         <button className="sm:hidden h-[65px] w-full bg-light-yellow text-dark-green text-2xl font-semibold">
           繼續逛逛
         </button>
