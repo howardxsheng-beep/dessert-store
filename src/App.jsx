314c3d39f40df13877d0a0d577036bb3b8ec8bd8
ho <howardxsheng@gmail.com>
2025-10-28 09:46:43 +0800

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ function App() {
       {/* <SiteHeader/> */}
       {/* <Product/> */}
       {/* <Home/> */}
-      {/* <Login/> */}
+      <Login/>
       {/* <Cart/> */}
       {/* <Shipping/> */}
       {/* <CheckoutSuccess/> */}
-      <Payment/>
+      {/* <Payment/> */}
       {/* <MailDist/> */}
       
     </>
